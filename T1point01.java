bbd7b5db1656e67c931ef6ecee3adf72116762f5
20103414 <101540098+20103414@users.noreply.github.com>
2022-04-11 02:19:20 +0800

    Setor Tugas

---
+++ T1point01.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1point01;
+
+/**
+ *
+ * @author user
+ */
+public class T1point01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[][]nama =
+        {
+            {"Brody","Dio","Jotaro","Nagisa","Karma"},
+                        
+        };
+        System.out.println("Nama Mahasiswa : "+(nama[0][0]));
+        System.out.println("Nama Mahasiswa : "+(nama[0][1]));
+        System.out.println("Nama Mahasiswa : "+(nama[0][2]));
+        System.out.println("Nama Mahasiswa : "+(nama[0][3]));
+        System.out.println("Nama Mahasiswa : "+(nama[0][4]));
+    }
+    
+}
---
+++ T1point02.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1point02;
+
+/**
+ *
+ * @author user
+ */
+public class T1point02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         String[][]nama =
+        {
+            {"Adi 127647 Teknik Informatika ","Mika 129883 Sistem Komputer ","Budi 120495 Sistem Komputer ","Bunga 123489 Teknik Informatika"},
+                        
+        };
+        System.out.println((nama[0][0]));
+        System.out.println((nama[0][1]));
+        System.out.println((nama[0][2]));
+        System.out.println((nama[0][3]));
+    }
+    
+}
